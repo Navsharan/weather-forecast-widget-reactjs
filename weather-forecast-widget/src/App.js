2fe152bcc0fc4233436da07bcfb9077d29378c77
Navi Cheema <navi@i-motor.com.au>
2021-01-31 17:50:36 +1100

    Get weather icon based on data from API.

--- weather-forecast-widget/src/App.js
+++ weather-forecast-widget/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'weather-icons/css/weather-icons.css';
 import Weather from './components/weather.component';
 
+// Location code
 const woeid = 1105779;
 class App extends React.Component {
   constructor() {
@@ -12,7 +11,7 @@ class App extends React.Component {
     this.state = {
       city: undefined,
       country: undefined,
-      icon: undefined,
+      icon_abbr: undefined,
       main: undefined,
       celsius: undefined,
       temp_min: undefined,
@@ -26,6 +25,7 @@ class App extends React.Component {
     this.getWeather();
   }
 
+  // Get weather data from MetaWeather API.
   getWeather = async () => {
     const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`);
 
@@ -36,6 +36,7 @@ class App extends React.Component {
     this.setState({
       city: response.title,
       country: response.parent.title,
+      icon_abbr: response.consolidated_weather[1].weather_state_abbr,
       celsius: parseInt(response.consolidated_weather[0].the_temp),
       temp_min: parseInt(response.consolidated_weather[0].min_temp),
       temp_max: parseInt(response.consolidated_weather[0].max_temp),
@@ -51,6 +52,7 @@ class App extends React.Component {
         <Weather 
         city={this.state.city}
         country={this.state.country}
+        icon_abbr={this.state.icon_abbr}
         temp_celsius={this.state.celsius}
         temp_min={this.state.temp_min}
         temp_max={this.state.temp_max}
--- weather-forecast-widget/src/components/weather.component.jsx
+++ weather-forecast-widget/src/components/weather.component.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
 
 const Weather = (props) => {
+
+    // URL to get weather icons from MetaWeather.
+    const icon_url = `https://www.metaweather.com/static/img/weather/${props.icon_abbr}.svg`;
+
     return (
         <div className="container">
             <div className="cards">
-                <h1>{props.city}, {props.country}</h1>
-                <h4 className="py-4">
-                    <i className="wi wi-day-sunny display-1"></i>
-                </h4>
+                <h1>{props.city}</h1>
+                <h5>{props.country}</h5>
+                <img className="py-4" src={icon_url} alt="weather icon"/>
                 <h2 className="py-2">{props.temp_celsius}&deg;C</h2>
-                
-                {/** Show min and max temp */}
-                {tempMinMax(props.temp_min, props.temp_max)}
-
+                <h5 className="py-1">{props.temp_min}&deg;C / {props.temp_max}&deg;C</h5>
                 <h5 className="py-1">{props.description}</h5>
                 <p className="py-1">Wind: {props.windspeed}km/h {props.wind_direction}</p>
             </div>
         </div>
-    )
-}
-
-function tempMinMax(min,max){
-    return (
-        <h5>
-            <span className="px-4">{min}&deg;C / {max}&deg;C</span>
-        </h5>
     )
 }
 
